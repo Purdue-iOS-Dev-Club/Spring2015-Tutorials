2c7f71feec826056a5401fb1a3b3ef86a932f50c
George Lo <twgeolo@gmail.com>
2015-02-18 21:18:31 -0500

    Finished Flappy Bird

--- Flappy Bird/Flappy Bird/GameScene.swift
+++ Flappy Bird/Flappy Bird/GameScene.swift
@@ -17,21 +17,40 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     let pipeTexture2: SKTexture!
     let moveAndRemovePipes: SKAction!
     
+    var pipes: SKNode!
+    
+    var score: Int = 0
+    let scoreLabel: SKLabelNode!
+    
     // 0000 0001
     let birdCategory = UInt32(1 << 0)  //0001
     let worldCategory = UInt32(1 << 1) // 0010
     let pipeCategory = UInt32(1 << 2) //0100
+    let scoreCategory = UInt32(1 << 3) // 1000
+    
+    var canRestart = false
     
     let moving: SKNode!
     
     override init(size: CGSize) {
         super.init(size: size)
         
+        canRestart = false
+        
         self.physicsWorld.gravity = CGVectorMake(0, -5)
         self.physicsWorld.contactDelegate = self
         
+        scoreLabel = SKLabelNode(fontNamed: "Avenir")
+        scoreLabel.position = CGPointMake(CGRectGetMidX(self.frame), 100)
+        scoreLabel.zPosition = 100
+        scoreLabel.text = "\(score)"
+        self.addChild(scoreLabel)
+        
         moving = SKNode()
         self.addChild(moving)
+        
+        pipes = SKNode()
+        moving.addChild(pipes)
         
         // Background color
         let skyBlueColor = SKColor(red: 135.0/255, green: 206.0/255, blue: 250.0/255, alpha: 1)
@@ -101,6 +120,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
         self.addChild(birdNode!)
         
+        // Physics Body for ground
         let dummy = SKNode()
         dummy.position = CGPointMake(0, groundTexture.size().height)
         dummy.physicsBody = SKPhysicsBody(rectangleOfSize: CGSizeMake(self.frame.width, groundTexture.size().height * 2))
@@ -108,6 +128,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         dummy.physicsBody?.categoryBitMask = worldCategory
         self.addChild(dummy)
         
+        // Load Pipe Images to SKTexture
         pipeTexture1 = SKTexture(imageNamed: "Pipe1")
         pipeTexture1.filteringMode = SKTextureFilteringMode.Nearest
         pipeTexture2 = SKTexture(imageNamed: "Pipe2")
@@ -131,6 +152,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         
         let y = arc4random() % UInt32(self.frame.size.height / 3.0)
         
+        // Generating the first pipe
         let pipe1 = SKSpriteNode(texture: pipeTexture1)
         pipe1.setScale(2.0)
         pipe1.position = CGPointMake(0, CGFloat(y))
@@ -140,6 +162,7 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         pipe1.physicsBody?.contactTestBitMask = birdCategory
         pipePair.addChild(pipe1)
         
+        // Generating the second pipe
         let pipe2 = SKSpriteNode(texture: pipeTexture2)
         pipe2.setScale(2.0)
         pipe2.position = CGPointMake(0, CGFloat(y) + pipe1.size.height + CGFloat(verticalPipeGap))
@@ -149,9 +172,17 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
         pipe2.physicsBody?.contactTestBitMask = birdCategory
         pipePair.addChild(pipe2)
         
+        let contactNode = SKNode()
+        contactNode.position = CGPointMake(pipe1.size.width + birdNode!.size.width / 2, CGRectGetMidY(self.frame))
+        contactNode.physicsBody = SKPhysicsBody(rectangleOfSize: CGSizeMake(pipe2.size.width, self.frame.size.height))
+        contactNode.physicsBody?.dynamic = false
+        contactNode.physicsBody?.categoryBitMask = scoreCategory
+        contactNode.physicsBody?.contactTestBitMask = birdCategory
+        pipePair.addChild(contactNode)
+        
         pipePair.runAction(moveAndRemovePipes)
         
-        moving.addChild(pipePair)
+        pipes.addChild(pipePair)
     }
 
     required init?(coder aDecoder: NSCoder) {
@@ -160,9 +191,29 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
         if moving.speed > 0 {
+            // Apply physics to make the bird fly
             birdNode?.physicsBody?.velocity = CGVectorMake(0, 0)
             birdNode?.physicsBody?.applyImpulse(CGVectorMake(0, 4))
+        } else if canRestart == true {
+            resetGame()
         }
+    }
+    
+    func resetGame() {
+        birdNode!.position = CGPointMake(self.frame.width / 4, CGRectGetMidY(self.frame))
+        birdNode!.physicsBody?.velocity = CGVectorMake(0, 0)
+        birdNode!.physicsBody?.collisionBitMask = worldCategory | pipeCategory
+        birdNode?.speed = 1.0
+        birdNode?.zRotation = 0.0
+        
+        canRestart = false
+        
+        pipes.removeAllChildren()
+        
+        moving.speed = 1
+        
+        score = 0
+        scoreLabel.text = "\(score)"
     }
     
     func clamp(#min: CGFloat, max: CGFloat, value: CGFloat) -> CGFloat {
@@ -175,26 +226,36 @@ class GameScene: SKScene, SKPhysicsContactDelegate {
     }
     
     override func update(currentTime: CFTimeInterval) {
-        birdNode!.zRotation = self.clamp(min: -1, max: 0.8, value: (birdNode!.physicsBody!.velocity.dy * (birdNode!.physicsBody!.velocity.dy < 0 ? 0.003 : 0.001)))
+        if moving.speed > 0 {
+            // Make the bird facing upward/downward
+            birdNode!.zRotation = self.clamp(min: -1, max: 0.8, value: (birdNode!.physicsBody!.velocity.dy * (birdNode!.physicsBody!.velocity.dy < 0 ? 0.003 : 0.001)))
+        }
     }
     
     func didBeginContact(contact: SKPhysicsContact) {
         if moving.speed > 0 {
-            moving.speed = 0
-            
-            self.removeActionForKey("flash")
-            self.runAction(SKAction.sequence([
-                SKAction.repeatAction(SKAction.sequence([
-                    SKAction.runBlock({
-                        self.backgroundColor = UIColor.redColor()
-                    }),
-                    SKAction.waitForDuration(0.05),
-                    SKAction.runBlock({
-                        self.backgroundColor = SKColor(red: 135.0/255, green: 206.0/255, blue: 250.0/255, alpha: 1)
-                    }),
-                    SKAction.waitForDuration(0.05)
-                ]), count: 4)
-            ]))
+            if contact.bodyA.categoryBitMask & scoreCategory == scoreCategory || contact.bodyB.categoryBitMask & scoreCategory == scoreCategory {
+                score++
+                scoreLabel.text = "\(score)"
+            } else {
+                moving.speed = 0
+                // Telling user the game has ended
+                self.removeActionForKey("flash")
+                self.runAction(SKAction.sequence([
+                    SKAction.repeatAction(SKAction.sequence([
+                        SKAction.runBlock({
+                            self.backgroundColor = UIColor.redColor()
+                        }),
+                        SKAction.waitForDuration(0.05),
+                        SKAction.runBlock({
+                            self.backgroundColor = SKColor(red: 135.0/255, green: 206.0/255, blue: 250.0/255, alpha: 1)
+                        }),
+                        SKAction.waitForDuration(0.05)
+                        ]), count: 4), SKAction.runBlock({
+                            self.canRestart = true
+                        })
+                    ]))
+            }
         }
     }
 }
